3221cd8baa274f444fdf72ef9676a4104d31de31
Stephane-dot-hub <stephane.kalanquin@gmail.com>
2026-01-06 10:39:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
 
+const SUPABASE_URL = 'https://isrxjzaphaciwdaxbylt.supabase.co'
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzcnhqemFwaGFjaXdkYXhieWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2ODI5NDAsImV4cCI6MjA4MzI1ODk0MH0.mPkEHEETgb4DMi4_LGQRkzIVlz6QoaLNvt6BXXSGoZQ'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
